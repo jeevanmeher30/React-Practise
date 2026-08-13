372dd7e1ad31115a23ea332effb3b48201f6fe2b
Jeevan Meher <jeevanmeher30@gmail.com>
2026-08-13 12:34:14 +0530

    Login Fix

--- src/pages/Log.jsx
+++ src/pages/Log.jsx
@@ -1,18 +1,80 @@
 import React from 'react'
 import "../style/Log.css"
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
 const Log = () => {
+  let [User , setUser] = useState('')
+  let [Pass , setPass] = useState('')
+
+  let NameRegex = /^[A-Za-z ]+$/
+  let EmailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[A-Za-z]{2,}$/
+  let PasswordRegex = /^[a-zA-Z0-9]{8,}$/
+  
+
+  let Url = "http://localhost:5000/user";
+  let Navigate = useNavigate();
+
+  const validate=()=>{
+
+    if(!User || !Pass){
+      alert("fields cannot be empty")
+      return false
+    }
+
+    if(!EmailRegex.test(User) || !NameRegex.test(User)){
+      alert("Email should be valid");
+      return false;
+    }
+
+    if(!PasswordRegex.test(Pass)){
+      alert("Password should be valid");
+      return false;
+    }
+    return true;
+
+
+  }
+const loginUser = (x) =>{
+  x.preventDefault();
+
+
+  if(!validate){
+    return;
+  }
+
+
+
+      axios.get(Url).then((res) => {
+      let users = res.data;
+      console.log(users)
+      let Result = users.find((user) => user.Email === User && user.Password === Pass);
+
+      if(Result){
+        alert("user login successfull");
+        Navigate("/")
+      }
+      else {
+        alert("invalid username or password");
+      }
+      
+    })
+
+
+
+  console.log(" Yo Login")
+}
+  
   return (
     <div className='log-con'>
-        <h1>Login here</h1>
-        <div>
-        <label htmlFor="">Username</label>
-        <input type="text" />
-        <label htmlFor="">Password</label>
-        <input type="text" />
+        <form action="" onSubmit={loginUser} >
 
-        <button>Submit</button>
-        </div>
+          <input type='text' placeholder='username or email' onChange={(x) => {setUser(x.target.value)}}/>
+          <input type='password' placeholder='password' onChange={(x) => {setPass(x.target.value)}}/>
+
+          <button type='submit'>login</button>
+        </form>
         
         
 
